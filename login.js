0000aaf815adb1289bea06bb76665976d45c6881
global47 <165831934+global47@users.noreply.github.com>
2024-12-06 16:01:37 -0800

    Update login.js | changed to roman's client ID

--- login.js
+++ login.js
@@ -2,7 +2,8 @@ const redirectUri = "https://cmpt-276-fall-2024.github.io/Euterpe/";
 const authEndpoint = "https://accounts.spotify.com/authorize";
 
 //const clientId = "7fcfb8b43cfa49c395412083cf51bf27";
-const clientId = "a2db107627e644a3b34c1ce7ed8a1ea8";
+//const clientId = "a2db107627e644a3b34c1ce7ed8a1ea8";
+const clientId = "2a6ec9abb39c4b0bbcc6815f91bb9ffc";
 // replace your clientID to the above, then you can log in Euterpe succcessly. Don't delete other's clientId, make them to comment.
 
 const scopesArray = [
